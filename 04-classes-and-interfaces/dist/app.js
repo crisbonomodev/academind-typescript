001a5ff83a8726c5e9aefdb95474b3e61e0c86e9
Cristian Bonomo <cristian.a.bonomo@gmail.com>
2021-07-18 10:39:28 -0300

    initial class setup

--- 04-classes-and-interfaces/dist/app.js
+++ 04-classes-and-interfaces/dist/app.js
@@ -1,3 +1,21 @@
 "use strict";
-console.log('Hello!');
+// Uso de clases en Typescript
+class Department {
+    // Constructor
+    constructor(name) {
+        this.name = name;
+    }
+    //Getters/Setters
+    get Name() {
+        return this.name;
+    }
+    set Name(value) {
+        this.name = value;
+    }
+}
+const salesDepartment = new Department('Sales');
+console.log(salesDepartment.Name);
+salesDepartment.Name = 'Marketing';
+console.log(salesDepartment.Name);
+console.log(salesDepartment);
 //# sourceMappingURL=app.js.map
--- 04-classes-and-interfaces/src/app.ts
+++ 04-classes-and-interfaces/src/app.ts
@@ -1,1 +1,26 @@
-console.log('Hello!');
+// Uso de clases en Typescript
+class Department {
+    //Props
+    private name: string;
+
+    // Constructor
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    //Getters/Setters
+    public get Name() : string {
+        return this.name;
+    }
+    
+    public set Name(value : string) {
+        this.name = value;
+    }
+
+}
+
+const salesDepartment = new Department('Sales');
+console.log(salesDepartment.Name);
+salesDepartment.Name = 'Marketing';
+console.log(salesDepartment.Name);
+console.log(salesDepartment);
