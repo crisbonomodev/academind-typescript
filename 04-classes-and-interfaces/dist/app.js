b118e61c7bdb8887fa403a88132defaa259efbf6
Cristian Bonomo <cristian.a.bonomo@gmail.com>
2021-07-18 18:41:33 -0300

    clases

--- 04-classes-and-interfaces/dist/app.js
+++ 04-classes-and-interfaces/dist/app.js
@@ -1,21 +1,37 @@
 "use strict";
 // Uso de clases en Typescript
 class Department {
+    //Props
+    //private name: string;
     // Constructor
-    constructor(name) {
+    //podemos declarar las propiedades tambien en el constructor
+    // el modificador readonly nos permite setear el parametro como solo lectura una vez inicializado
+    constructor(id, name) {
+        this.id = id;
         this.name = name;
+        // this.id = id; // Podemos comentar este sector de codigo ya que al declararlos en los
+        // this.name = name; // Parametros del constructor, tambien los inicalizamos. 
     }
     //Getters/Setters
+    get Id() {
+        return this.id;
+    }
     get Name() {
         return this.name;
     }
     set Name(value) {
         this.name = value;
     }
+    //Metodos
+    // devuelve el nombre del departamento formateado
+    describe() {
+        console.log(`Department: ${this.name}`);
+    }
 }
-const salesDepartment = new Department('Sales');
-console.log(salesDepartment.Name);
+const salesDepartment = new Department(1, 'Sales');
+salesDepartment.describe();
+// const salesCopy = {describe: salesDepartment.describe}; //como no hay referencia al objeto
+// salesCopy.describe(); //Esto devuelve undefined
 salesDepartment.Name = 'Marketing';
-console.log(salesDepartment.Name);
-console.log(salesDepartment);
+salesDepartment.describe();
 //# sourceMappingURL=app.js.map
--- 04-classes-and-interfaces/src/app.ts
+++ 04-classes-and-interfaces/src/app.ts
@@ -1,14 +1,21 @@
 // Uso de clases en Typescript
 class Department {
     //Props
-    private name: string;
+    //private name: string;
 
     // Constructor
-    constructor(name: string) {
-        this.name = name;
+    //podemos declarar las propiedades tambien en el constructor
+    // el modificador readonly nos permite setear el parametro como solo lectura una vez inicializado
+    constructor(private readonly id: number, private name: string) {
+        // this.id = id; // Podemos comentar este sector de codigo ya que al declararlos en los
+        // this.name = name; // Parametros del constructor, tambien los inicalizamos. 
     }
 
     //Getters/Setters
+    public get Id() : number {
+        return this.id;
+    }
+
     public get Name() : string {
         return this.name;
     }
@@ -17,10 +24,20 @@ class Department {
         this.name = value;
     }
 
+    //Metodos
+
+    // devuelve el nombre del departamento formateado
+    describe(this: Department) { //Colocamos la referencia al objeto para evitar errores
+        console.log( `Department: ${this.name}`);
+    }
+
 }
 
-const salesDepartment = new Department('Sales');
-console.log(salesDepartment.Name);
+const salesDepartment = new Department(1,'Sales');
+salesDepartment.describe();
+
+// const salesCopy = {describe: salesDepartment.describe}; //como no hay referencia al objeto
+// salesCopy.describe(); //Esto devuelve undefined
+
 salesDepartment.Name = 'Marketing';
-console.log(salesDepartment.Name);
-console.log(salesDepartment);
+salesDepartment.describe();
