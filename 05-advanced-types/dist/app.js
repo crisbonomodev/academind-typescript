1f32fe08887bcb1ea89085371f847ea9ebbe1dd3
Cristian Bonomo <cristian.a.bonomo@gmail.com>
2021-07-19 17:50:04 -0300

    intersection types

--- 05-advanced-types/dist/app.js
+++ 05-advanced-types/dist/app.js
@@ -1,3 +1,9 @@
 "use strict";
-console.log('Hey!');
+// Intersection Types
+// Podemos usar multiples tipos para crear una interseccion 
+const user = {
+    name: 'Cristian',
+    privileges: ['create-server'],
+    startDate: new Date()
+};
 //# sourceMappingURL=app.js.map
--- 05-advanced-types/src/app.ts
+++ 05-advanced-types/src/app.ts
@@ -1,1 +1,27 @@
-console.log('Hey!');
+// Intersection Types
+// Podemos usar multiples tipos para crear una interseccion 
+
+type Admin = {
+    name: string;
+    privileges: string[];
+};
+
+type Employee = {
+    name: string;
+    startDate: Date;
+};
+
+type SuperUser = Admin & Employee;
+
+const user: SuperUser = {
+    name: 'Cristian',
+    privileges: ['create-server'],
+    startDate: new Date() 
+};
+
+// Tambien podemos hacer interseccion de tipos mas simples
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
